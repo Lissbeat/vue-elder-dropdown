06f42e4e0b1abdabadd6b7218a37b0db9c48182f
Carsten Jacobsen <car.jacobsen@gmail.com>
2020-05-16 11:56:55 +0200

    fix(component): make popper load lazy to improve performance

--- src/component.vue
+++ src/component.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <div class="elder-dropdown">
+  <div class="elder-dropdown" :class="{ 'elder-dropdown--inactive': !instance }">
     <div class="elder-dropdown__trigger" ref="element" @click="toggle">
       <slot />
     </div>
     <div
-      v-show="visible"
+      v-if="visible"
       ref="target"
       class="elder-dropdown__content"
       @click="closeOnClick && toggle()"
@@ -35,35 +35,44 @@ export default {
     }
   },
   methods: {
-    toggle: function() {
-      this.visible = !this.visible
-      this.instance.update()
+    toggle: function () {
+      if (this.instance) return this.destroy()
+      return this.init()
     },
-    clickAway: function(e) {
+    clickAway: function (e) {
       if (!this.$el.contains(e.target)) this.toggle()
+    },
+    init() {
+      this.visible = true
+      this.$nextTick(() => {
+        this.instance = createPopper(this.$refs.element, this.$refs.target, {
+          placement: this.placement,
+          modifiers: [
+            {
+              name: 'offset',
+              options: {
+                offset: [0, 8],
+              },
+            },
+          ],
+        })
+      })
+    },
+    destroy() {
+      if (!this.instance) return
+      this.instance.destroy()
+      this.instance = null
+      this.visible = false
     },
   },
   watch: {
-    visible: function(val) {
+    visible: function (val) {
       if (val) window.addEventListener('click', this.clickAway)
       else window.removeEventListener('click', this.clickAway)
     },
   },
-  mounted: function() {
-    this.instance = createPopper(this.$refs.element, this.$refs.target, {
-      placement: this.placement,
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [0, 8],
-          },
-        },
-      ],
-    })
-  },
-  beforeDestroy: function() {
-    if (this.instance) this.instance.destroy()
+  beforeDestroy() {
+    this.destroy()
   },
 }
 </script>
@@ -75,11 +84,17 @@ export default {
   position: relative;
   display: flex;
 
+  &--inactive .elder-dropdown__content {
+    pointer-events: none;
+    visibility: hidden;
+  }
+
   &__trigger {
     display: inherit;
   }
 
   &__content {
+    position: absolute;
     box-shadow: 0 5px 30px -5px rgba(0, 0, 0, 0.2);
     border-radius: $border-radius;
     overflow: hidden;
